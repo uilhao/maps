3cac6e159afb8610a6cbf64216376aded75f9a02
William Pickens <william@pickensdesigns.com>
2020-07-22 16:11:01 -0700

    footer

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 
 	<footer class="container py-5">
 		<div class="row">
-			<div class="col-12 col-md">
+			<div class="col-2">
 				<div class="logo"></div>
 				<small class="d-block mb-3 text-muted">© 2017-2020</small>
 			</div>
