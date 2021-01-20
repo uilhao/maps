75e88b2826a05098ed6ba41912100bd2c5ab3b6e
William <wpickens@myersmediagroup.com>
2021-01-20 14:50:53 -0800

    tweaks

--- template-parts/header/banner.php
+++ template-parts/header/banner.php
@@ -17,7 +17,7 @@ $wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
 	<div class="banner position-relative overflow-hidden text-center bg-light">
 		<div class="col-md-6 p-lg-6 mx-auto">
 			<h1 class="display-5 font-weight-normal"><?php echo get_bloginfo('description'); ?></h1>
-			<p class="lead font-weight-normal">Calças para mulheres com medidas de pernas além dos limites!!</p>
+			<p class="lead font-weight-normal">Calças para mulheres com medidas de pernas além dos limites!!!</p>
 			<a class="btn btn-dark" href="/sobre-a-maps/">Saiba mais</a>
 		</div>
 	</div>
